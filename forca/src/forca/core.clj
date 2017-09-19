bc446df69baa96a516463d4734c8dd4e87a90446
Arthur Fernandes Araujo <arthur.araujo@cds.eb.mil.br>
2017-09-19 12:45:26 -0300

    Very simple decrement function

--- forca/src/forca/core.clj
+++ forca/src/forca/core.clj
@@ -1,6 +1,20 @@
 (ns forca.core
   (:gen-class))
 
+(def total-de-vidas 6)
+
+(defn perdeu [] (print "Perdeu"))
+
+(defn jogo [vidas] 
+	(if (= vidas 0)
+		(perdeu)
+		(do
+			(print vidas)
+			(jogo (- vidas 1))
+		)
+	)
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
