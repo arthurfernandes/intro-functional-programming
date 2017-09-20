355fe0e0ad37da5b53b435627aabc07fccf66632
Arthur Fernandes Araujo <arthurfernandes05@gmail.com>
2017-09-19 23:58:36 -0300

    Finished forca game

--- forca/src/forca/core.clj
+++ forca/src/forca/core.clj
@@ -2,9 +2,10 @@
   (:gen-class))
 
 (def total-de-vidas 6)
+(def palavra-secreta "MELANCIA")
 
-(defn perdeu [] (print "Você Perdeu!"))
-(defn ganhou [] (print "Você Ganhou!"))
+(defn perdeu [] (println "Você Perdeu!"))
+(defn ganhou [] (println "Você Ganhou!"))
 
 (defn letras-faltantes [palavra acertos]
 	(remove(fn [letra] (contains? acertos (str letra))) palavra)
@@ -17,23 +18,36 @@
 	(empty? (letras-faltantes palavra acertos))
 )
 
+(defn uppercase [letra] (.toUpperCase (str letra)))
+
 (defn acertou? [chute, palavra] (.contains palavra chute))
 
+(defn imprime-forca [vidas palavra acertos]
+	(println "Vidas " vidas)
+	(doseq [letra (seq palavra)]
+		(if (contains? acertos (str letra))
+			(print letra " ")
+			(print "_" " ")
+		))
+	(println))
+
 (defn jogo [vidas palavra acertos] 
+	(imprime-forca vidas palavra acertos)
 	(cond 
 		(= vidas 0) (perdeu)
 		(acertou-a-palavra-toda? palavra acertos) (ganhou)
 		:else 
-		(let [chute (le-letra!)]
+		(let [chute (uppercase (le-letra!))]
             (if (acertou? chute palavra)
                 (do
-                    (println "Acertou a letra!")
+                    (println "Acertou Mizeravi!")
                     (recur vidas palavra (conj acertos chute)))
                 (do
                     (println "Errou a letra! Perdeu vida!")
                     (recur (dec vidas) palavra acertos))))))
 
+(defn comeca-jogo [vidas palavra] (jogo vidas palavra #{}))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (comeca-jogo total-de-vidas palavra-secreta))
